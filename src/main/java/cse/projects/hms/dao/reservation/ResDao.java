2d71606aaacfd3b7339e85863e5c06504eabcc93
LEEJiMin003 <147581729+LEEJiMin003@users.noreply.github.com>
2023-11-17 16:48:22 +0900

    예약조회기능 구현완료 (#3)

--- src/main/java/cse/projects/hms/dao/reservation/ResDao.java
+++ src/main/java/cse/projects/hms/dao/reservation/ResDao.java
@@ -9,15 +9,11 @@ import cse.projects.hms.view.ReservaionCheck;
 import java.io.*;
 import java.util.*;
 
-
 /**
  *
  * @author 재진
  */
 public class ResDao {
-
-    String[] userInfo = null;
-
     public void insert(ResDto resDto) {
         // 예약 정보 저장하는 파일 입출력 코드
         String fileName = "data/UserData.txt";
@@ -29,69 +25,19 @@ public class ResDao {
             System.out.println("insert 오류");
         }
     }
-    
+
     public String[] readAll() {
         String fileName = "data/UserData.txt";
         File file = new File(fileName);
-        List<String> userDataList=new ArrayList<>();
-        try(BufferedReader output = new BufferedReader(new FileReader(file))){
+        List<String> userDataList = new ArrayList<>();
+        try (BufferedReader output = new BufferedReader(new FileReader(file))) {
             String line;
-            while((line=output.readLine())!=null){
+            while ((line = output.readLine()) != null) {
                 userDataList.add(line);
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("오류검출");
         }
         return userDataList.toArray(new String[0]);
-    }
-    public List<String> resroomCheck() { //예약된 객실호수만 읽는 메서드
-        String[] roomdata = null;
-        List<String> roomdataList = new ArrayList<>();
-        String fileName = "data/UserData.txt";
-        File file = new File(fileName);
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] row = line.split(",");
-                roomdataList.add(row[3]);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        roomdata = roomdataList.toArray(new String[0]);
-        return roomdataList;
-    }
-
-    public void roomSearch() { //검색된 객실호수의 고객정보 찾기 
-        ReservaionCheck ress = new ReservaionCheck();
-        int linecount=0;
-        int currentLineNumber=0;
-        String fileName = "data/UserData.txt";
-        File file = new File(fileName);
-        
-        try (BufferedReader br = new BufferedReader(new FileReader(file))){
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] row = line.split(",");
-                linecount++;
-                System.out.println(ress.txt_searchroomnum());
-                if(ress.txt_searchroomnum()==row[3]){
-                    break;
-                }
-            }
-            
-            while ((line = br.readLine()) != null) {
-                currentLineNumber++;
-
-                // 현재 라인 번호가 읽고자 하는 라인 번호와 일치하는 경우
-                if (currentLineNumber == linecount) {
-                    //System.out.println(line);
-                    break;
-                }
-            }
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
     }
 }
