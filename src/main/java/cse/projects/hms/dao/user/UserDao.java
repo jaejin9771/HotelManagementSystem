5f2eb5c5a6eb4c274cb23bbfa76ca2623d0310a5
Bae Jae Jin <crystal10806@naver.com>
2023-11-17 14:58:24 +0900

    UserDao,LoginDto 수정 (#2)

--- src/main/java/cse/projects/hms/dao/user/UserDao.java
+++ src/main/java/cse/projects/hms/dao/user/UserDao.java
@@ -24,30 +24,27 @@ public class UserDao {
         File file = new File(fileName);
 
         try {
-            
-            BufferedReader br = new BufferedReader(new FileReader(file));
-                    
-            String line;
-            while ((line = br.readLine()) != null) {                
-                String[] userInfo = line.split(","); // txt파일에 저장되어있는 아이디와 비밀번호를 나눠서 저장.
-                 
 
-                System.out.println("cse.projects.hms.dao.user.UserDao.findByIdAndPw() : 입력한 값 : "+loginDto.getId()+","+loginDto.getPw());                
-                System.out.println("cse.projects.hms.dao.user.UserDao.findByIdAndPw() : 비교할 값 : "+userInfo[0]+","+userInfo[1]);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] userInfo = line.split(","); // txt파일에 저장되어있는 아이디와 비밀번호를 나눠서 저장.
+
                 // id pw 확인
                 if (loginDto.getId().equals(userInfo[0])) {
                     if (loginDto.getPw().equals(userInfo[1])) {
-                                              
+
                         userDto = new UserBuilder()
                                 .id(loginDto.getId())
                                 .pw(loginDto.getPw())
                                 .usertype(userInfo[2])
                                 .build();
                         return userDto;
-                      
                     }
                 }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
             return null;
